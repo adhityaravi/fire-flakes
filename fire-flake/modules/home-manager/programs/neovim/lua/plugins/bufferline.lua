21004a83a1b2eec53bcecd6272ecd2b45fb4ac1e
Adhitya Ravi <adhitya.ravi@canonical.com>
2025-05-02 00:03:19 +0200

    chore[nvim]: modernize bufferline

--- fire-flake/modules/home-manager/programs/neovim/lua/plugins/bufferline.lua
+++ fire-flake/modules/home-manager/programs/neovim/lua/plugins/bufferline.lua
@@ -1,39 +1,43 @@
 require("bufferline").setup({
   options = {
     mode = "buffers",
-    numbers = "ordinal",
-    diagnostics = "nvim_lsp",
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    modified_icon = "●",
-    close_icon = "",
-    separator_style = "slant", -- or "padded_slant", "thick"
+    numbers = "none",
+    separator_style = "slant", -- options: "thin", "slant", "padded_slant"
     always_show_bufferline = true,
-    sort_by = 'insert_after_current', -- New buffers appear next to current
-    enforce_regular_tabs = true,      -- Makes tabs more uniform width
+    show_buffer_close_icons = true,
+    show_close_icon = false,
+    color_icons = true,
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level)
+      local icon = (level:match("error") and " ") or (level:match("warning") and " ") or " "
+      return " " .. icon .. count
+    end,
     offsets = {
       {
         filetype = "NvimTree",
         text = "File Explorer",
-        text_align = "center",
+        highlight = "Directory",
+        text_align = "left",
         separator = true,
       },
     },
     hover = {
       enabled = true,
-      delay = 200,
+      delay = 150,
       reveal = { "close" }
     },
-    diagnostics_indicator = function(count, level, diagnostics_dict)
-      local s = ""
-      for e, n in pairs(diagnostics_dict) do
-        local sym = (e == "error" and " ") or (e == "warning" and " ") or " "
-        s = s .. n .. sym
+    style_preset = {
+      require("bufferline").style_preset.minimal,
+    },
+    custom_areas = {
+      right = function()
+        return {
+          { text = " " .. vim.fn.FugitiveHead(), fg = "#61afef" },
+        }
       end
-      return s
-    end,
+    },
   },
+  highlights = require("catppuccin.groups.integrations.bufferline").get(),
 })
 
 -- keybindings
