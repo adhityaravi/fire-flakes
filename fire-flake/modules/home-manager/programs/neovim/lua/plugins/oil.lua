af7eecca501c661fd110bd674f27eea2fe6b9481
Adhitya Ravi <34714491+adhityaravi@users.noreply.github.com>
2025-06-04 10:57:12 +0200

    feat[nvim]: tweak oil defaults

--- fire-flake/modules/home-manager/programs/neovim/lua/plugins/oil.lua
+++ fire-flake/modules/home-manager/programs/neovim/lua/plugins/oil.lua
@@ -1,2 +1,9 @@
-require("oil").setup{}
+require("oil").setup({
+  default_file_explorer = true,
+  view_options = {
+    show_hidden = true,
+  },
+  skip_confirm_for_simple_edits = true,
+  delete_to_trash = true,
+})
 
