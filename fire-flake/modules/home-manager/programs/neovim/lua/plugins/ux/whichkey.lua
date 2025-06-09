8bdabc1e2bda8cd8aea42a94291ab68bc50742fc
Adhitya Ravi <adhitya.ravi@canonical.com>
2025-06-10 01:13:17 +0200

    feat[nvim]: curosorified nvim with goose

--- fire-flake/modules/home-manager/programs/neovim/lua/plugins/ux/whichkey.lua
+++ fire-flake/modules/home-manager/programs/neovim/lua/plugins/ux/whichkey.lua
@@ -1,25 +1,25 @@
 local wk = require("which-key")
 wk.setup({
-  preset = "helix",
-  win = {
-    border = "rounded",
-  },
+	preset = "helix",
+	win = {
+		border = "rounded",
+	},
 })
 
 wk.add({
-  { "<leader>h", group = "hydras", icon = "󰕚" },
-  { "<leader>g", group = "git", icon = "󰊢" },
-  { "<leader>gh", group = "github", icon = "" },
-  { "<leader>b", group = "buffers", icon = "󰈚" },
-  { "<leader>l", group = "lsp", icon = "󰒕" },
-  { "<leader>f", group = "fuzzy-find", icon = "󰭎" },
-  { "<leader>a", group = "ai-assist", icon = "" },
-  { "<leader>t", group = "toggles", icon = "" },
-  { "<leader>d", group = "debug", icon = "" },
-  { "<leader>r", group = "find-replace", icon = "" },
-  { "<leader>p", group = "grapple", icon = "󰄛" },
-  { "<leader>q", group = "quick-fix", icon = "" },
-  { "<leader>R", group = "REST", icon = "󰖆" },
-  { "<leader>o", group = "obsidian", icon = "󰍉" },
-  { "<leader>A", group = "goose", icon = "🪿" },
+	{ "<leader>h", group = "hydras", icon = "󰕚" },
+	{ "<leader>g", group = "git", icon = "󰊢" },
+	{ "<leader>gh", group = "github", icon = "" },
+	{ "<leader>b", group = "buffers", icon = "󰈚" },
+	{ "<leader>l", group = "lsp", icon = "󰒕" },
+	{ "<leader>f", group = "fuzzy-find", icon = "󰭎" },
+	{ "<leader>c", group = "copilot", icon = "" },
+	{ "<leader>t", group = "toggles", icon = "" },
+	{ "<leader>d", group = "debug", icon = "" },
+	{ "<leader>r", group = "find-replace", icon = "" },
+	{ "<leader>p", group = "grapple", icon = "󰄛" },
+	{ "<leader>q", group = "quick-fix", icon = "" },
+	{ "<leader>R", group = "REST", icon = "󰖆" },
+	{ "<leader>o", group = "obsidian", icon = "󰍉" },
+	{ "<leader>a", group = "goose-ai", icon = "🪿" },
 })
